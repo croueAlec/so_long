11989ec43c64d372addd47800c2d64fc7aeab583
Alec Croue <acroue@bess-f4r3s3.clusters.42paris.fr>
2024-01-12 12:48:20 +0100

    added textures and defines to go with

--- includes/so_long_defines.h
+++ includes/so_long_defines.h
@@ -6,7 +6,7 @@
 /*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 15:54:38 by acroue            #+#    #+#             */
-/*   Updated: 2024/01/11 15:59:38 by acroue           ###   ########.fr       */
+/*   Updated: 2024/01/12 12:13:18 by acroue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,40 @@
 # define MAX_HEIGHT 16
 
 /* Textures */
-# define ASSETS 5
 # define TILE_SIZE 32
+# define ASSETS 14
+# define DEFAULT_TEXTURE 0
+# define PLAYER_TEXTURE 1
+# define EXIT_TEXTURE 2
+# define COIN_TEXTURE 3
+# define UPPER_LEFT_WALL 4
+# define LOWER_LEFT_WALL 5
+# define UPPER_RIGHT_WALL 6
+# define LOWER_RIGHT_WALL 7
+# define UPPER_WALL 8
+# define LOWER_WALL 9
+# define LEFT_WALL 10
+# define RIGHT_WALL 11
+# define TILE 12
+# define TILE_VARIANT 13
+# define LONE_WALL 14
+/* Texture paths */
+# define DEFAULT_TEXTURE_PATH "textures/default_texture.xpm"
+# define PLAYER_TEXTURE_PATH "textures/player.xpm"
+# define EXIT_TEXTURE_PATH "textures/exit.xpm"
+# define COIN_TEXTURE_PATH "textures/default_texture.xpm"
+# define UPPER_LEFT_WALL_PATH "textures/upper_left_wall.xpm"
+# define LOWER_LEFT_WALL_PATH "textures/lower_left_wall.xpm"
+# define UPPER_RIGHT_WALL_PATH "textures/upper_right_wall.xpm"
+# define LOWER_RIGHT_WALL_PATH "textures/lower_right_wall.xpm"
+# define UPPER_WALL_PATH "textures/upper_wall.xpm"
+# define LOWER_WALL_PATH "textures/lower_wall.xpm"
+# define LEFT_WALL_PATH "textures/left_wall.xpm"
+# define RIGHT_WALL_PATH "textures/right_wall.xpm"
+# define TILE_PATH "textures/tile.xpm"
+# define TILE_VARIANT_PATH "textures/tile_variant.xpm"
+# define LONE_WALL_PATH "textures/lone_wall.xpm"
+
 
 /* Error messages */
 # define BAD_MAP_PATH "Bad map path"
